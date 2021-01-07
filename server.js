8f5d1c606e6fd51f6dea97b1a9a6233744779ce3
AmanSethi13 <AmanSethi13@users.noreply.github.com>
2021-01-07 16:16:49 +0530

    adding database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-metric-02404',
         user: 'postgres',
         password: '1234',
         database: 'facerecog'
