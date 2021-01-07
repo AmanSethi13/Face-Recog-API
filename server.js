270e969ea7d0aa995ae5d55790e68109f62e2263
AmanSethi13 <AmanSethi13@users.noreply.github.com>
2021-01-07 15:16:17 +0530

    modified cors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://mysterious-bastion-30381.herokuapp.com/');
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     res.setHeader('Access-Control-Allow-Credentials', true);
